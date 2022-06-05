13063d711b153d6a590dc9476e6071be3eaaecfb
Hatem El-Sebaaly <hatemstartups@gmail.com>
2022-06-05 10:30:21 -0500

    fix: fixed pipeline

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app import routers
 
 service = FastAPI(
     title="fast api example",
-    description="Example Python API.",
+    description="Example Python API V1.",
     version="v1",
 )
 
